d6ff5e9c407090333729494ccd1f88b6d667d806
Holly Henske <hcover333@gmail.com>
2025-06-20 14:46:12 -0500

    responsivenes done, problems fixed. Need active and hover effects

--- app.js
+++ app.js
@@ -9,3 +9,25 @@ menuToggle.addEventListener('click', () => {
     overlay.classList.toggle('active');
 });
 
+const mainImage = document.getElementById("mainImage");
+const images = [
+    "./images/image-product-1.jpg",
+    "./images/image-product-2.jpg",
+    "./images/image-product-3.jpg",
+    "./images/image-product-4.jpg",
+];
+let currentIndex = 0;
+
+function showImage(index) {
+    mainImage.src = overlay,ages[index];
+}
+
+document.querySelector(".arrow.prev").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+});
+
+document.querySelector(".arrow.next").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+});
